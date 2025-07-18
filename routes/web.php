79d518cd7f4ba8375a51abea83b64a0385354a18
dangsangn <dangsangn6147@gmail.com>
2025-07-18 15:47:58 +0000

    login and register page

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,9 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+
+return view('admin.index');
+
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
